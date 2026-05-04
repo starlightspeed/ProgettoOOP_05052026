bf7f836743a15b32783a7d89bae63de48e1b99a1
Rome-NotBeepBoop <jeromef.taccaban@gmail.com>
2026-05-04 14:06:09 +0200

    removed the main file and restored it to its original prof written author

---
+++ src/main/java/model/Fiera.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Fiera {
+    private final String titolo;
+
+    public Fiera(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+    
+
+}
